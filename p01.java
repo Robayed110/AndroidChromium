6c020eaae55d0914db02da3fdc1fa20f525f64d1
Robayed110 <126972094+Robayed110@users.noreply.github.com>
2023-03-12 02:06:06 +0600

    ok

---
+++ p01.java
@@ -0,0 +1,27 @@
+package com.example.calculator;
+
+public class p01 {
+    private String name;
+    private int roll;
+
+    public p01(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
